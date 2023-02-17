c7a21b81ceba2ba0f7afa588bb64aa45c0116797
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-17 09:18:34 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     servoGrip += 5
-    servoGrip = Math.constrain(servoGrip, 0, 90)
+    servoGrip = Math.constrain(servoGrip, 90, 180)
 })
 input.onButtonPressed(Button.B, function () {
     servoGrip += -5
-    servoGrip = Math.constrain(servoGrip, 0, 90)
+    servoGrip = Math.constrain(servoGrip, 90, 180)
 })
 let servoGrip = 0
 basic.showLeds(`
@@ -14,7 +14,7 @@ basic.showLeds(`
     . . . . #
     . . . . #
     `)
-servoGrip = 45
+servoGrip = 135
 basic.forever(function () {
     pins.servoWritePin(AnalogPin.P16, servoGrip)
     basic.pause(100)
