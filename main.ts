859c5b7569d58bd6e168bea25530b581caa0796d
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-20 12:11:17 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,4 @@
-radio.onReceivedValue(function (name, value) {
-    if (name == "goBase") {
-        speedBase = Math.map(value, -90, 90, -2, 2)
-    } else if (name == "goLeft") {
-        speedLeft = Math.map(value, -90, 90, -2, 2)
-    } else if (name == "goRight") {
-        speedRight = Math.map(value, -90, 90, -2, 2)
-    } else if (name == "goGrip") {
-        speedGrip = Math.map(value, -90, 90, -2, 2)
-    }
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         # # # # #
         # . . . #
@@ -25,13 +14,31 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     servoGrip_target = 135
     basic.pause(2000)
     smoothFactor = currentSmooth
-    basic.showLeds(`
-        # # . . .
-        . . # # #
-        # # . . #
-        . . . . #
-        . . . . #
-        `)
+    isIdle = !(isIdle)
+    if (isIdle) {
+        basic.showIcon(IconNames.Ghost)
+    } else {
+        basic.showLeds(`
+            # # . . .
+            . . # # #
+            # # . . #
+            . . . . #
+            . . . . #
+            `)
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (!(isIdle)) {
+        if (name == "goBase") {
+            speedBase = Math.map(value, -90, 90, -2, 2)
+        } else if (name == "goLeft") {
+            speedLeft = Math.map(value, -90, 90, -2, 2)
+        } else if (name == "goRight") {
+            speedRight = Math.map(value, -90, 90, -2, 2)
+        } else if (name == "goGrip") {
+            speedGrip = Math.map(value, -90, 90, -2, 2)
+        }
+    }
 })
 let thisVal = 0
 let currentSmooth = 0
@@ -40,6 +47,7 @@ let servoGrip_target = 0
 let servoRight_target = 0
 let servoLeft_target = 0
 let servoBase_target = 0
+let isIdle = false
 let speedBase = 0
 let speedLeft = 0
 let speedRight = 0
@@ -49,6 +57,7 @@ speedRight = 0
 speedLeft = 0
 speedBase = 0
 radio.setGroup(250)
+isIdle = false
 basic.showLeds(`
     # # . . .
     . . # # #
@@ -66,39 +75,43 @@ let servoRight = 90
 let servoGrip = 135
 smoothFactor = 0.5
 basic.forever(function () {
-    servoBase_target = Math.constrain(servoBase_target, 45, 135)
-    servoLeft_target = Math.constrain(servoLeft_target, 15, 165)
-    servoRight_target = Math.constrain(servoRight_target, 0, 180)
-    servoGrip_target = Math.constrain(servoGrip_target, 90, 180)
-    servoBase = smoothFactor * servoBase + (1 - smoothFactor) * servoBase_target
-    servoLeft = smoothFactor * servoLeft + (1 - smoothFactor) * servoLeft_target
-    servoRight = smoothFactor * servoRight + (1 - smoothFactor) * servoRight_target
-    servoGrip = smoothFactor * servoGrip + (1 - smoothFactor) * servoGrip_target
-    pins.servoWritePin(AnalogPin.P13, servoBase)
-    pins.servoWritePin(AnalogPin.P14, servoLeft)
-    pins.servoWritePin(AnalogPin.P15, servoRight)
-    pins.servoWritePin(AnalogPin.P16, servoGrip)
+    if (!(isIdle)) {
+        servoBase_target = Math.constrain(servoBase_target, 45, 135)
+        servoLeft_target = Math.constrain(servoLeft_target, 50, 160)
+        servoRight_target = Math.constrain(servoRight_target, 45, 160)
+        servoGrip_target = Math.constrain(servoGrip_target, 90, 180)
+        servoBase = smoothFactor * servoBase + (1 - smoothFactor) * servoBase_target
+        servoLeft = smoothFactor * servoLeft + (1 - smoothFactor) * servoLeft_target
+        servoRight = smoothFactor * servoRight + (1 - smoothFactor) * servoRight_target
+        servoGrip = smoothFactor * servoGrip + (1 - smoothFactor) * servoGrip_target
+        pins.servoWritePin(AnalogPin.P13, servoBase)
+        pins.servoWritePin(AnalogPin.P14, servoLeft)
+        pins.servoWritePin(AnalogPin.P15, servoRight)
+        pins.servoWritePin(AnalogPin.P16, servoGrip)
+    }
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        thisVal = (pins.analogReadPin(AnalogPin.P0) - 507) / 512
-        speedBase = 2 * thisVal
-        thisVal = (pins.analogReadPin(AnalogPin.P1) - 507) / 512
-        speedLeft = 2 * thisVal
-        thisVal = (pins.analogReadPin(AnalogPin.P2) - 507) / 512
-        speedRight = 2 * thisVal
-        speedGrip = 0
-    } else if (input.buttonIsPressed(Button.B)) {
-        thisVal = (pins.analogReadPin(AnalogPin.P0) - 507) / 512
-        speedGrip = 2 * thisVal
-        speedBase = 0
-        speedLeft = 0
-        speedRight = 0
-    } else {
-    	
+    if (!(isIdle)) {
+        if (input.buttonIsPressed(Button.A)) {
+            thisVal = (pins.analogReadPin(AnalogPin.P0) - 507) / 512
+            speedBase = 2 * thisVal
+            thisVal = (pins.analogReadPin(AnalogPin.P1) - 507) / 512
+            speedLeft = 2 * thisVal
+            thisVal = (pins.analogReadPin(AnalogPin.P2) - 507) / 512
+            speedRight = 2 * thisVal
+            speedGrip = 0
+        } else if (input.buttonIsPressed(Button.B)) {
+            thisVal = (pins.analogReadPin(AnalogPin.P0) - 507) / 512
+            speedGrip = 2 * thisVal
+            speedBase = 0
+            speedLeft = 0
+            speedRight = 0
+        } else {
+        	
+        }
+        servoBase_target = servoBase_target - speedBase
+        servoLeft_target = servoLeft_target + speedLeft
+        servoRight_target = servoRight_target + speedRight
+        servoGrip_target = servoGrip_target + speedGrip
     }
-    servoBase_target = servoBase_target + speedBase
-    servoLeft_target = servoLeft_target + speedLeft
-    servoRight_target = servoRight_target + speedRight
-    servoGrip_target = servoGrip_target + speedGrip
 })
